4ef491985b2638f6e97fb655c6cfcf00ee62ae0e
abdellimed <57357370+abdellimed@users.noreply.github.com>
2024-01-04 21:51:58 +0100

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,15 +1,11 @@
 # Import libraries
-
 import argparse
 import glob
 import os
 import mlflow
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
-
 # define functions
 def main(args):
     # TO DO: enable autologging
@@ -22,10 +18,10 @@ def main(args):
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)    
 def get_csvs_df(path):
     if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+        raise RuntimeError(f"Cannot use non-existent path provided:{path}")
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+        raise RuntimeError(f"No CSV files found in provided data path:{path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 # TO DO: add function to split data
 def split_data(df, test_size=0.2):
